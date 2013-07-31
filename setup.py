c4c255238362d80bdc20f00b0b8dfbdce72c3ac5
Steve Pulec <spulec@gmail.com>
2013-07-30 20:17:32 -0400

    another 1.5 fix

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ for dirpath, dirnames, filenames in os.walk(src_dir):
 
 setup(
     name=app_name,
-    version="1.0.0.4",
+    version="1.0.0.5",
     description='Django chronograph application.',
     author='Weston Nielson',
     author_email='wnielson@gmail.com',
